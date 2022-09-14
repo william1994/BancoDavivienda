da8dc4a2ad67bbe5b816b6ffe0bbc796dbe5169d
William Diaz <williamefrain1994@gmail.com>
2022-09-14 13:36:11 -0600

    Creacion del proyecto

---
+++ Desarrollador .Net Core- Banco/Desarrollador .Net Core- Banco/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] MyArray =  { 1, 2, 1, 3, 3, 1, 2, 1, 5, 1 };
+int n = MyArray.Length;
+
+bool[] visited = new bool[n];
+
+for (int i = 1; i <= 5; i++)
+{
+    
+    int count = 0;
+
+    
+    for (int j = 0; j < n; j++)
+    {
+        if (visited[j] == true)
+            continue;
+
+        if (MyArray[j] == i)
+        {
+            visited[j] = true;
+            count++;
+        }
+    }
+
+
+    Console.Write(i + ": ");
+    for (int k = 0; k < count; k++)
+    {
+        Console.Write('*');
+    }
+    Console.WriteLine();
+}
